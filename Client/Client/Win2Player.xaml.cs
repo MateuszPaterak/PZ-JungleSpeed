ae41e49af2d7bee10599b2d7530ceb69b039b31b
MateuszPaterak <mzt@onet.eu>
2016-04-07 22:51:13 +0200

    completed: position of players

--- Client/Client/Win2Player.xaml.cs
+++ Client/Client/Win2Player.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,24 +24,76 @@ namespace Client
         public Win2Player()
         {
             InitializeComponent();
+            //
+            CreatePlayers(6, "Player");
+            ChangePlayerCard(0, 1);
+            ChangePlayerCard(1, 4);
+            ChangePlayerCard(2, 3);
+            ChangePlayerCard(3, 2);
+            ChangePlayerCard(4, 2);
+            ChangePlayerCard(5, 4);
 
-            LabPlayer1.Content = "sdfs";
-
-            CreatePlayer();
+            ChangeNamePlayer("Ja", 0);
+            ChangeNamePlayer("Jan", 3);
         }
 
-        private void CreatePlayer() //-s
+        private Image[] _imgPlayerCardArray = new Image[10];
+        private Label[] _labPlayerName = new Label[10];
+        public enum NameOfBack {palm, wood, bokeh}
+
+        private void ChangePlayerCard(byte nrPlayer, byte cardNr)
         {
-            var cart = new Image
-            {
-                Width = 100,
-                Height = 100,
-                Margin = new Thickness(280, 280, 0, 0),
-                Source = new BitmapImage(new Uri(@"/Pictures/totem.png", UriKind.Relative))
-            };
-            GridPlayer.Children.Add(cart);
-
+         _imgPlayerCardArray[nrPlayer].Source =
+            new BitmapImage(
+                new Uri(@"/Pictures/Cards/" + cardNr.ToString() + ".png", UriKind.Relative));
         }
+
+        private void CreatePlayers(byte playersNumber, string name)
+        {
+            var distance = (2 * Math.PI) / playersNumber;
+            
+            for (byte num=0; num<playersNumber; num++)
+            {
+                var radius = 0.7*(GridPlayer.Height - 10);
+                var pos = distance*num;
+                var posY = -Math.Sin(pos + 1.5*Math.PI) * radius;
+                var posX = -Math.Cos(pos + 0.5*Math.PI) * radius;
+
+                var card = new Image
+                    {
+                        Name = "ImgCardPlayer" + num.ToString(),
+                        Width = 100,
+                        Height = 100,
+                        Margin =  new Thickness(posX, posY+30, 0, 0),
+                    };
+                
+                var lab = new Label
+                {
+                    Content = name+num,
+                    Margin = new Thickness(posX, posY-120, 0, 0),
+                    Background = Brushes.DarkGray, 
+                    Height = 30,
+                    Width = 100,
+                };
+
+                _imgPlayerCardArray[num] = card;
+                _labPlayerName[num] = lab;
+                GridPlayer.Children.Add(card);
+                GridPlayer.Children.Add(lab);
+            }
+        }
+
+        private void ChangeNamePlayer(string name, byte numer)
+        {
+            _labPlayerName[numer].Content = name;
+        }
+
+        /*private void ChangeBackround(String name)
+        {
+            ImgBackground.ImageSource = 
+                    new BitmapImage(
+                        new Uri(@"/Pictures/" + name + ".jpg", UriKind.Relative));
+        }*/
 
     }
 }
