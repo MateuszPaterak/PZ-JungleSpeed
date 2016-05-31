0b75a8dad3f70fc1143323e2f8806c78674ae290
wilq94 <paba.lech@poczta.fm>
2016-05-31 21:40:22 +0200

    pokoj.cs

---
+++ AplikacjaSerwerowa/AplikacjaSerwerowa/Pokoj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaSerwerowa
+{
+    public class Pokoj
+    {
+        public int id;
+        public string nazwa;
+
+        public Pokoj(int id, string nazwa)
+        {
+            this.id = id;
+            this.nazwa = nazwa;            
+        }
+    }
+}
