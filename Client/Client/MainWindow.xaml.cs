1d396335f69433af1ab625858798cdc2fb09ec59
MateuszPaterak <mzt@onet.eu>
2016-06-16 01:45:01 +0200

    added simple help button-option

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -176,5 +176,15 @@ namespace Client
         {
 
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string help = "Autorzy:\nMateusz Paterak\nPaweł Wilczyński\n\n";
+            help += "Główne okno aplikacji po uruchomieniu.\nDostępne są dwa główne przyciski umożliwiające stworzenie nowego pokoju, lub dołączenie do istniejącego (utworzonego wcześniej przez innego gracza). Gracz może wprowadzić swoją nazwę przez zmianę zawartości pola “Nick”. Po prawej stronie dostępne są trzy przyciski pozwalające na kontrolowanie rozgrywki. Na górze ekranu znajduje się menu pozwalające otworzyć okno pomocy oraz wybrać inny adres IP serwera gry.";
+            help += "\n\nOkno pozwalające utworzyć nowy pokój gracza. \nW lewym górnym rogu istnieje możliwość wpisania własnej nazwy pokoju, jaka będzie się pojawiać na liście wyszukiwań dostępnych pokoi. Przycisk “Utwórz pokój” powoduje wysłanie komunikatu do serwera z nazwą pokoju, oraz od tej chwili inni gracze mogą dołączać od pokoju. Przycisk “Usuń pokój” powoduje wysłanie do serwera gry informacji że nie chcemy już kontynuować gry poprzez tworzenie bieżącego pokoju, a usuwamy go wraz z ewentualnymi graczami którzy byli przypisani do niego. “Anuluj tworzenie pokoju” powoduje usunięcie pokoju, oraz dodatkowo zamyka bieżące okno. Przycisk “Rozpocznij grę” powoduje zamknięcie bieżącego okna oraz wysłanie do serwera komunikatu z rozkazem rozpoczęcia gry. Dostępne są 3 listy, które wyświetlają wszystkie pokoje znajdujące się na serwerze, oraz listę graczy wewnątrz naszego pokoju którzy są dopisani do rozgrywki, oraz którzy tego nie zrobili.";
+            help += "\n\nOkno pozwalające dołączyć do istniejącego pokoju. \nDostępne są główne 3 listy.Pierwsza od lewej strony wyświetla listę aktualnie dostępnych pokoi.Należy na nią kliknąć, a następnie wybrać przycisk “Dołącz do pokoju”. Kolejne dwie listy wyświetlają graczy którzy są obserwatorami gry lub, znajdują się w kolejce do uruchomienia gry. Przy pomocy dodatkowych przycisków możliwie jest dołączanie do tych list oraz wypisywanie się z nich. Zamknięcie okna przyciskiem w prawym górnym rogu powoduje automatyczne wypisanie się z pokoju oraz zamknięcie okna.";
+            help += "\n\nGracze na przemiennie podnoszą karty. W przypadku gdy któryś z dwóch graczy posiada ten sam symbol (kolor nie ma znaczenia), podnosi totem klikając w przycisk “Weź totem”";
+            MessageBox.Show(help);
+        }
     }
 }
